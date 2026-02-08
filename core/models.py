36fc74c8a18e0f7200fde808cb718a974559ccad
Faith Etornam <faithgbadegbe1@gmail.com>
2026-02-08 20:25:57 +0000

    Added the SMSUser Model for monitoring User actions

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class SMSUser(models.Model):
+    phone_number = models.CharField(max_length=15, unique=True, db_index=True, help_text="Format +233")
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    credits = models.IntegerField(default=10, help_text="Number of free queries remaining")
+    is_active = models.BooleanField(default=True, help_text='If False, user receives no replies')
+
+    class Meta:
+        verbose_name = 'SMS User'
+        verbose_name_plural = 'SMS Users'
+
+    def __str__(self):
+        return f"{self.phone_number} {self.credits} credits left"
